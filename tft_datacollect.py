5d0b999d195deb799166abdbacd7c4d20c767720
mrasska <45817556+mrasska@users.noreply.github.com>
2022-03-20 15:10:48 +0100

    Update tft_datacollect.py

--- tft_datacollect.py
+++ tft_datacollect.py
@@ -6,7 +6,7 @@ import pandas as pd
 import time
 
 header={
-    
+   
 }
 
 def connect(url, header): 
@@ -37,31 +37,44 @@ def db_player(db):
     df_db=pd.DataFrame(data)
     return df_db
 
-def get_puid_id(df_db, header): 
+def get_puid_id(df_db, header, name): 
     name_player=df_db['name_player']
     name_player=name_player.to_list()
 
     puid=[]
-    last_matches=[]
+    puuid=[]
+    id_match=[]
     for m in name_player:
         url_puid='https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/'+m
         req=connect(url_puid, header)
-        print(m)
+
+        #database df_db -> players, wins, losses
+        #database df_matches -> player, id_match        
         try :
             p=req['puuid']
             puid.append(p)
             
+            #creating df_matches
             url_id_match='https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/'+p+'/ids?count=50'
-            req=connect(url_id_match, header)
-            last_matches.append(req)
-            
+            req_m=connect(url_id_match, header)
+
+            for i in req_m : 
+                puuid.append(p)
+                id_match.append(i)
+
         except KeyError : 
             pdb.set_trace()
         time.sleep(2)
     
     df_db['puid']=puid
-    df_db['last_matches']=last_matches
-    return df_db
+
+    data={'puid':puuid, 'id_match': id_match}
+    df_match=pd.DataFrame(data)
+    df_match.to_csv(name, header=True, index=False)
+    
+    return df_db, df_match
+
+
 
 
 def type_db(header):
@@ -74,20 +87,21 @@ def type_db(header):
     if user_value=='C':
         req=connect(url_c, header)
         db=crawl_json(req)
-        name='leaderboard_chall.csv'
         df_db=db_player(db)
-        df_db=get_puid_id(df_db=df_db, header=header)
+        name_db_match='list_match_chall.csv'
+        df_db=get_puid_id(df_db=df_db, header=header, name=name_db_match)
+        name='leaderboard_chall.csv'
         df_db.to_csv(name, header=True, index=False)
 
     elif user_value=='GM':
         req=connect(url_gm, header)
         db=crawl_json(req)
-        name='leaderboard_gm.csv'
         df_db=db_player(db)
-        df_db=get_puid_id(df_db=df_db, header=header)
+        name_db_match='list_match_gm.csv'
+        df_db=get_puid_id(df_db=df_db, header=header, name=name_db_match)
+        name='leaderboard_gm.csv'
         df_db.to_csv(name, header=True, index=False)
 
     return print('Data collected!'), df_db
 
-df_db=type_db(header)
-pdb.set_trace()
+df_db=type_db(header)   
